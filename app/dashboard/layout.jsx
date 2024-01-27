c90bed7a2006d053cfe5d594a0650d4e725667b7
catherine <vuthi.esther@gmail.com>
2024-01-28 00:43:24 +0300

    Add menuItems

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -4,11 +4,11 @@ import Navbar from '../ui/dashboard/navbar/navbar'
 
 const Layout = ({ children }) => {
   return (
-    <div>
-      <div>
+    <div className='flex flex-row gap-3'>
+      <div className='basis-1/4 bg-slate-900 p-5'>
         <Sidebar />
       </div>
-      <div>
+      <div className='basis-3/4 bg-slate-900 p-5'>
         <Navbar />
         {children}
       </div>
--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import { MdDashboardCustomize, MdHardware, MdAnalytics, MdLogout } from 'react-icons/md'
+import { FaFileInvoiceDollar } from 'react-icons/fa'
+import { TbReport } from 'react-icons/tb'
+
+const menuItems = [
+  {
+    list: [
+      {
+        title: 'Dashboard',
+        path: '/dashboard',
+        icon: <MdDashboardCustomize />,
+      },
+      {
+        title: 'Products',
+        path: '/dashboard/products',
+        icon: <MdHardware />,
+      },
+      {
+        title: 'Invoices',
+        path: '/dashboard/invoices',
+        icon: <FaFileInvoiceDollar />,
+      },
+      {
+        title: 'Analytics',
+        path: '/dashboard/analytics',
+        icon: <MdAnalytics />,
+      },
+      {
+        title: 'Reports',
+        path: '/dashboard/reports',
+        icon: <TbReport />,
+      },
+      {
+        title: 'Logout',
+        path: '/',
+        icon: <MdLogout />,
+      },
+    ],
+  },
+]
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
+  return <div>Sidebar</div>
 }
 
 export default Sidebar
