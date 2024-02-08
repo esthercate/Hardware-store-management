b693546414f32a18dd240d936761a75960bf7c3c
catherine <vuthi.esther@gmail.com>
2024-02-08 23:15:50 +0300

    Card texts

--- app/ui/dashboard/card/card.jsx
+++ app/ui/dashboard/card/card.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { PiChartLineUpLight, PiChartLineDownLight } from 'react-icons/pi'
 
 const Card = () => {
   return (
-    <div>Card</div>
+    <div className='flex'>
+      <div className='texts flex flex-col gap-5'>
+        <span>Daily sales</span>
+        <span>Ksh. 24,000</span>
+        <span>Increased by 60%</span>
+      </div>
+      <div>
+        <PiChartLineUpLight />
+        {/* //!Todo conditionally display this icon */}
+        {/* <PiChartLineDownLight /> */}
+      </div>
+    </div>
   )
 }
 
