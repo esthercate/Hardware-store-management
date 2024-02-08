3dc05bc7daac1697d59781586f7f79c80070634e
catherine <vuthi.esther@gmail.com>
2024-02-08 23:05:15 +0300

    Add dashboard page styles

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -6,16 +6,20 @@ import Transaction from '../ui/dashboard/transaction/transaction'
 
 const Dashboard = () => {
   return (
-    <div className='wrapper'>
-      <div className='main flex flex-col gap-10'>
-        <div className='cards flex'>
+    <div className='wrapper flex gap-5 mt-5'>
+      <div className='main flex flex-col gap-10 w-full'>
+        <div className='cards flex justify-between gap-5'>
           <Card />
           <Card />
           <Card />
         </div>
-        <div className='flex'>
-          <Chart />
-          <RightBar />
+        <div className='flex flex-row'>
+          <div className='basis-3/4'>
+            <Chart />
+          </div>
+          <div className='basis-1/4'>
+            <RightBar />
+          </div>
         </div>
         <div>
           <Transaction />
