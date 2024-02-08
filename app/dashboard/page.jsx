4619eabb50a3feea8be343e1986f11c6b5a57f45
catherine <vuthi.esther@gmail.com>
2024-02-08 22:50:29 +0300

    Use dashboard ui components on page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Card from '../ui/dashboard/card/card'
+import Chart from '../ui/dashboard/chart/chart'
+import RightBar from '../ui/dashboard/rightbar/rightbar'
+import Transaction from '../ui/dashboard/transaction/transaction'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className='wrapper'>
+      <div className='main flex flex-col gap-10'>
+        <div className='cards flex'>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <div className='flex'>
+          <Chart />
+          <RightBar />
+        </div>
+        <div>
+          <Transaction />
+        </div>
+      </div>
+    </div>
   )
 }
 
