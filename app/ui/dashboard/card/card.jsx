f3006930891c77e5d08611c22c4e925072925f80
catherine <vuthi.esther@gmail.com>
2024-02-09 21:50:16 +0300

    Card changes

--- app/ui/dashboard/card/card.jsx
+++ app/ui/dashboard/card/card.jsx
@@ -3,14 +3,14 @@ import { PiChartLineUpLight, PiChartLineDownLight } from 'react-icons/pi'
 
 const Card = () => {
   return (
-    <div className='flex'>
-      <div className='texts flex flex-col gap-5'>
+    <div className='flex justify-between bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500'>
+      <div className='texts flex flex-col gap-4 '>
         <span>Daily sales</span>
         <span>Ksh. 24,000</span>
         <span>Increased by 60%</span>
       </div>
       <div>
-        <PiChartLineUpLight />
+        <PiChartLineUpLight size={25} />
         {/* //!Todo conditionally display this icon */}
         {/* <PiChartLineDownLight /> */}
       </div>
