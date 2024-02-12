c65425afeabe3d840bafbe85012a456d41ee3e3b
catherine <vuthi.esther@gmail.com>
2024-02-12 22:21:38 +0300

    Add tbody data

--- app/ui/dashboard/transaction/transaction.jsx
+++ app/ui/dashboard/transaction/transaction.jsx
@@ -13,7 +13,32 @@ const Transaction = () => {
             <td>Amount</td>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          <tr>
+            <td>John Doe</td>
+            <td>Paid</td>
+            <td>12.02.2024</td>
+            <td>Ksh. 2,000</td>
+          </tr>
+          <tr>
+            <td>Jane Doe</td>
+            <td>Paid</td>
+            <td>12.02.2024</td>
+            <td>Ksh. 2,000</td>
+          </tr>
+          <tr>
+            <td>Joni Doe</td>
+            <td>Paid</td>
+            <td>12.02.2024</td>
+            <td>Ksh. 2,000</td>
+          </tr>
+          <tr>
+            <td>Jose Doe</td>
+            <td>Paid</td>
+            <td>12.02.2024</td>
+            <td>Ksh. 2,000</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   )
