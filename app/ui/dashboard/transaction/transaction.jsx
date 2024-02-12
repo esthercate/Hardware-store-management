1922ec50c8e76e6077ebc6cea3e7cabc9ba9dc23
catherine <vuthi.esther@gmail.com>
2024-02-12 22:16:34 +0300

    Add thead

--- app/ui/dashboard/transaction/transaction.jsx
+++ app/ui/dashboard/transaction/transaction.jsx
@@ -5,7 +5,15 @@ const Transaction = () => {
     <div>
       <h2>Latest Transactions</h2>
       <table>
-        
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Status</td>
+            <td>Date</td>
+            <td>Amount</td>
+          </tr>
+        </thead>
+        <tbody></tbody>
       </table>
     </div>
   )
