c6a722713da4a18994fceade19d4b8510877c087
catherine <vuthi.esther@gmail.com>
2024-02-13 22:06:55 +0300

    Change table format

--- app/ui/dashboard/transaction/transaction.jsx
+++ app/ui/dashboard/transaction/transaction.jsx
@@ -1,7 +1,54 @@
+'use client'
+
 import { useMemo } from 'react'
 import { MantineReactTable, useMantineReactTable } from 'mantine-react-table'
 
 const data = [
+  {
+    name: {
+      firstName: 'Zachary',
+      lastName: 'Davis',
+    },
+    address: '261 Battle Ford',
+    city: 'Columbus',
+    state: 'Ohio',
+  },
+  {
+    name: {
+      firstName: 'Robert',
+      lastName: 'Smith',
+    },
+    address: '566 Brakus Inlet',
+    city: 'Westerville',
+    state: 'West Virginia',
+  },
+  {
+    name: {
+      firstName: 'Kevin',
+      lastName: 'Yan',
+    },
+    address: '7777 Kuhic Knoll',
+    city: 'South Linda',
+    state: 'West Virginia',
+  },
+  {
+    name: {
+      firstName: 'John',
+      lastName: 'Upton',
+    },
+    address: '722 Emie Stream',
+    city: 'Huntington',
+    state: 'Washington',
+  },
+  {
+    name: {
+      firstName: 'Nathan',
+      lastName: 'Harris',
+    },
+    address: '1 Kuhic Knoll',
+    city: 'Ohiowa',
+    state: 'Nebraska',
+  },
   {
     name: {
       firstName: 'Zachary',
@@ -76,15 +123,10 @@ const Transaction = () => {
     []
   )
 
-  const table = useMantineReactTable({
-    columns,
-    data,
-  })
-
   return (
     <div className='bg-slate-900 p-5 rounded-lg'>
       <h2 className='mb-5 font-light text-xl text-slate-400'>Latest Transactions</h2>
-      <MantineReactTable table={table} />;
+      <MantineReactTable columns={columns} data={data} />
     </div>
   )
 }
