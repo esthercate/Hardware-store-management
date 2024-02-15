67997fa458bb61715d08c534f9df077f97fcccb8
catherine <vuthi.esther@gmail.com>
2024-02-15 14:22:35 +0300

    Different gradient for each card

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -4,14 +4,30 @@ import Chart from '../ui/dashboard/chart/chart'
 import RightBar from '../ui/dashboard/rightbar/rightbar'
 import Transaction from '../ui/dashboard/transaction/transaction'
 
+const cardData = [
+  {
+    title: 'Daily sales',
+    amount: 24000,
+    change: '40%',
+  },
+  {
+    title: 'Weekly sales',
+    amount: 64000,
+    change: '60%',
+  },
+  {
+    title: 'Montly sales',
+    amount: 124000,
+    change: '80%',
+  },
+]
+
 const Dashboard = () => {
   return (
     <div className='wrapper flex gap-5 mt-5'>
       <div className='main flex flex-col gap-10 w-full'>
         <div className='cards flex justify-between gap-5'>
-          <Card />
-          <Card />
-          <Card />
+          <Card cardData={cardData} />
         </div>
         <div className='flex flex-row gap-5'>
           <div className='basis-3/4'>
--- app/ui/dashboard/card/card.jsx
+++ app/ui/dashboard/card/card.jsx
@@ -1,21 +1,29 @@
 import React from 'react'
 import { PiChartLineUpLight, PiChartLineDownLight } from 'react-icons/pi'
 
-const Card = () => {
+const Card = ({ cardData }) => {
   return (
-    <div className='flex flex-1 justify-between p-5 rounded-lg gap-5 cursor-pointer shadow-lg bg-gradient-to-r from-violet-500 to-fuchsia-500'>
-      <div className='texts flex flex-col gap-5'>
-        <span className='text-lg'>Daily sales</span>
-        <span className='text-xl font-medium'>Ksh. 24,000</span>
-        <span className='font-light text-sbase'>Increased by 80%</span>
-      </div>
-      <div>
-        <PiChartLineUpLight size={25} />
-        {/* //!Todo conditionally display this icon */}
-        {/*CurrentPercentage > previousPercentage ? <PiChartLineUpLight size={25} /> : <PiChartLineDownLight />  */}
-        {/* <PiChartLineDownLight /> */}
-      </div>
-    </div>
+    <>
+      {cardData.map((data, index) => (
+        <div
+          key={data.title}
+          className={`flex flex-1 justify-between p-5 rounded-lg gap-5 cursor-pointer shadow-lg ${
+            index === 0 ? 'bg-gradient-to-r from-indigo-500 to-emerald-500 ' : index === 1 ? 'bg-gradient-to-r from-violet-500 to-fuchsia-500' : index === 2 ? 'bg-gradient-to-r from-cyan-500 to-blue-500' : ''
+          }`}>
+          <div className='texts flex flex-col gap-5'>
+            <span className='text-lg'>{data.title}</span>
+            <span className='text-xl font-medium'>{data.amount}</span>
+            <span className='font-light text-sbase'>Increased by {data.change}</span>
+          </div>
+          <div>
+            <PiChartLineUpLight size={25} />
+            {/* //!Todo conditionally display this icon */}
+            {/*CurrentPercentage > previousPercentage ? <PiChartLineUpLight size={25} /> : <PiChartLineDownLight />  */}
+            {/* <PiChartLineDownLight /> */}
+          </div>
+        </div>
+      ))}
+    </>
   )
 }
 
