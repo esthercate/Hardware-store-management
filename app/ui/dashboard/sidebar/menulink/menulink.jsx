7de9f287c5eeefb96a2fa14059b717a8c31ae067
catherine <vuthi.esther@gmail.com>
2024-02-16 14:49:12 +0300

    Change font weight on hover

--- app/ui/dashboard/sidebar/menulink/menulink.jsx
+++ app/ui/dashboard/sidebar/menulink/menulink.jsx
@@ -10,7 +10,7 @@ const MenuLink = ({ items }) => {
     <>
       {items.map((item, index) => (
         <li key={index}>
-          <Link href={item.path} className={`flex py-7 px-4 items-center gap-4 my-5 rounded-lg hover:text-violet-700 ${pathname === item.path ? 'bg-slate-800 text-violet-700' : 'bg-none'}`}>
+          <Link href={item.path} className={`flex py-7 px-4 items-center gap-4 my-5 rounded-lg hover:text-fuchsia-600 ${pathname === item.path ? 'bg-slate-800 text-fuchsia-600 font-semibold' : 'bg-none'}`}>
             {item.icon} {item.title}
           </Link>
         </li>
