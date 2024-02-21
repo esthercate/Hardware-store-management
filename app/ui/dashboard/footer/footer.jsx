d2d3121b9dd2c9f9b14b301aedfd3fcc9a230894
catherine <vuthi.esther@gmail.com>
2024-02-21 21:23:30 +0300

    Add copyright icon

--- app/ui/dashboard/footer/footer.jsx
+++ app/ui/dashboard/footer/footer.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { MdCopyright } from 'react-icons/md'
 
 const Footer = () => {
   return (
-    <div className='flex justify-center items-center font-bold text-lg mt-7 text-slate-400'>
+    <div className='flex justify-center items-center gap-2 font-bold text-lg mt-7 text-slate-400'>
+      <MdCopyright />
       <div>All rights reserved.</div>
     </div>
   )
