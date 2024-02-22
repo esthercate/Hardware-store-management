72d463c1d867d78cd03f8f487eefd421ae545ff9
catherine <vuthi.esther@gmail.com>
2024-02-22 22:28:57 +0300

    Change icon size

--- app/ui/dashboard/footer/footer.jsx
+++ app/ui/dashboard/footer/footer.jsx
@@ -4,7 +4,7 @@ import { MdCopyright } from 'react-icons/md'
 const Footer = () => {
   return (
     <div className='flex justify-center items-center gap-2 font-bold text-lg mt-7 text-slate-400'>
-      <MdCopyright />
+      <MdCopyright size={20} />
       <div>All rights reserved.</div>
     </div>
   )
