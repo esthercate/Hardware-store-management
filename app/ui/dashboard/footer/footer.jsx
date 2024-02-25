48dc1635e12606a7e9a50678eaf7a2b5310ad7d7
catherine <vuthi.esther@gmail.com>
2024-02-25 22:04:12 +0300

    Text changes

--- app/ui/dashboard/footer/footer.jsx
+++ app/ui/dashboard/footer/footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className='flex justify-center items-center gap-3 font-bold text-lg mt-7 text-slate-400'>
       <MdCopyright size={20} />
-      <div>All rights reserved.</div>
+      <div>All Rights Reserved.</div>
     </div>
   )
 }
