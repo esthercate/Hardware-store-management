d2b01e7569ca3621328e1a4f63970facea7a6010
catherine <vuthi.esther@gmail.com>
2024-02-27 22:33:57 +0300

    Add button style

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import Search from '@app/ui/dashboard/search/search'
+import Search from '../../ui/dashboard/search/search'
 import Link from 'next/link'
 
 const ProductsPage = () => {
   return (
-    <div className='container'>
-      <Search placeholder={'Search a product...'} />
-      <Link href='/dashboard/products/add'>
-        <button>Add New</button>
-      </Link>
+    <div className='bg-slate-900 p-5 rounded-lg mt-5'>
+      <div className='flex items-center justify-between'>
+        <Search placeholder={'Search a product...'} />
+        <Link href='/dashboard/products/add'>
+          <button className='p-2.5 text-white bg-green-600 border-none rounded-md'>Add New</button>
+        </Link>
+      </div>
     </div>
   )
 }
