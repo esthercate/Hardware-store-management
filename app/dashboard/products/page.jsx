d2eb5c684c94ee5fdc95e3536ed1e018b824d8ae
catherine <vuthi.esther@gmail.com>
2024-02-28 21:31:20 +0300

    table row data

--- app/dashboard/products/page.jsx
+++ app/dashboard/products/page.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Search from '../../ui/dashboard/search/search'
 import Link from 'next/link'
+import Image from 'next/image'
 import { MdOutlineAdd } from 'react-icons/md'
 
 const ProductsPage = () => {
@@ -21,10 +22,33 @@ const ProductsPage = () => {
           <td>Description</td>
           <td>Buying Price</td>
           <td>Selling Price</td>
-          <td>created At</td>
+          <td>Created At</td>
           <td>Stock</td>
           <td>Action</td>
         </thead>
+        <tbody>
+          <tr>
+            <td>Cement</td>
+            <td>50kgs bag</td>
+            <td>Ksh. 600</td>
+            <td>Ksh. 750</td>
+            <td>28.02.2024</td>
+            <td>32 bags</td>
+            <td>
+              <div>
+                <Link href={'/'}>
+                  <button>View</button>
+                </Link>
+                <Link href={'/'}>
+                  <button>Delete</button>
+                </Link>
+                <Link href={'/'}>
+                  <button>Update</button>
+                </Link>
+              </div>
+            </td>
+          </tr>
+        </tbody>
       </table>
     </div>
   )
