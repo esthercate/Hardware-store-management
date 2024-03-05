65b01fdd3f7970a15ddaa7b64a733637a0ba1d31
catherine <vuthi.esther@gmail.com>
2024-03-05 21:32:14 +0300

    Add header

--- app/dashboard/invoices/page.jsx
+++ app/dashboard/invoices/page.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const InvoicesPage = () => {
-  return <div>Invoices generation Page</div>
+  return (
+    <div>
+      <h1>Invoices generation Page</h1>
+    </div>
+  )
 }
 
 export default InvoicesPage
