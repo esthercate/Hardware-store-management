8ddbbe699b1708e81183f479364fe7d1758f0a92
catherine <vuthi.esther@gmail.com>
2024-03-08 22:32:54 +0300

    Form input nd role select

--- app/dashboard/users/add/page.jsx
+++ app/dashboard/users/add/page.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 const AddUserPage = () => {
   return (
     <div className='bg-slate-900 p-5 rounded-lg mt-5'>
-      <form></form>
+      <form>
+        <div className='flex justify-between gap-5 mb-4'>
+          <input className='p-5 w-1/2 bg-transparent rounded-md border-2 border-slate-700' type='text' placeholder='Name' name='name' required />
+          <select name='role' id='role' className='p-5 w-1/2 bg-transparent rounded-md border-2 border-slate-700'>
+            <option value='general'>Choose a Role</option>
+            <option value='role1'>Role 1</option>
+            <option value='role2'>Role 2</option>
+            <option value='role3'>Role 3</option>
+            <option value='role4'>Role 4</option>
+          </select>
+        </div>
+      </form>
     </div>
   )
 }
