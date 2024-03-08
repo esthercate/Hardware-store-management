978e4850587e93ce40db1c202a766910df942b33
catherine <vuthi.esther@gmail.com>
2024-03-08 22:46:35 +0300

    add role

--- app/dashboard/users/add/page.jsx
+++ app/dashboard/users/add/page.jsx
@@ -6,6 +6,10 @@ const AddUserPage = () => {
       <form>
         <div className='flex justify-between gap-5 mb-4'>
           <input className='p-5 w-1/2 bg-transparent rounded-md border-2 border-slate-700' type='text' placeholder='Name' name='name' required />
+          <input className='p-5 w-1/2 bg-transparent rounded-md border-2 border-slate-700' type='number' placeholder='Phone Number' name='phoneNo' required />
+        </div>
+        <div className='flex justify-between gap-5 mb-4'>
+          <input className='p-5 w-1/2 bg-transparent rounded-md border-2 border-slate-700' type='email' placeholder='Email' name='email' required />
           <select name='role' id='role' className='p-5 w-1/2 bg-transparent rounded-md border-2 border-slate-700'>
             <option value='general'>Choose a Role</option>
             <option value='role1'>Role 1</option>
@@ -13,10 +17,6 @@ const AddUserPage = () => {
             <option value='role3'>Role 3</option>
             <option value='role4'>Role 4</option>
           </select>
-        </div>
-        <div className='flex justify-between gap-5 mb-4'>
-          <input className='p-5 w-1/2 bg-transparent rounded-md border-2 border-slate-700' type='number' placeholder='Phone Number' name='phoneNo' required />
-          <input className='p-5 w-1/2 bg-transparent rounded-md border-2 border-slate-700' type='email' placeholder='Email' name='email' required />
         </div>
       </form>
     </div>
