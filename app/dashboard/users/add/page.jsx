4308ee0bea17aea6fb097c9ea4a1813ed149817a
catherine <vuthi.esther@gmail.com>
2024-03-08 22:54:36 +0300

    Add user status

--- app/dashboard/users/add/page.jsx
+++ app/dashboard/users/add/page.jsx
@@ -10,12 +10,21 @@ const AddUserPage = () => {
         </div>
         <div className='flex justify-between gap-5 mb-4'>
           <input className='p-5 w-1/2 bg-transparent rounded-md border-2 border-slate-700' type='email' placeholder='Email' name='email' required />
+          <input className='p-5 w-1/2 bg-transparent rounded-md border-2 border-slate-700' type='text' placeholder='Upload Image' name='image' required />
+        </div>
+        <div className='flex justify-between gap-5 mb-4'>
           <select name='role' id='role' className='p-5 w-1/2 bg-transparent rounded-md border-2 border-slate-700'>
             <option value='general'>Choose a Role</option>
             <option value='role1'>Role 1</option>
             <option value='role2'>Role 2</option>
             <option value='role3'>Role 3</option>
             <option value='role4'>Role 4</option>
+          </select>
+          <select name='status' id='status' className='p-5 w-1/2 bg-transparent rounded-md border-2 border-slate-700'>
+            <option value='general'>Choose Status</option>
+            <option value='active'>Active</option>
+            <option value='inactive'>Inactive</option>
+            <option value='action'>Action</option>
           </select>
         </div>
       </form>
