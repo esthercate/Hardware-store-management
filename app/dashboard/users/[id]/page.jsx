34ef4cf398cb810647b0a91f5ae6d2da4cee4308
catherine <vuthi.esther@gmail.com>
2024-03-10 21:57:46 +0300

    Add view user page

--- app/dashboard/users/[id]/page.jsx
+++ app/dashboard/users/[id]/page.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Image from 'next/image'
 
 const SingleUserPage = () => {
   return (
-    <div>Single user page</div>
+    <div className='flex'>
+      <div>
+        <Image className='rounded-full' src='/profile.png' alt='avater' width='150' height='150' />
+      </div>
+      <div>User Info</div>
+    </div>
   )
 }
 
