d08bad60631c709e248805e2af926ef1e40fd26b
catherine <vuthi.esther@gmail.com>
2024-03-15 21:54:06 +0300

    make form full screen

--- app/dashboard/users/[id]/page.jsx
+++ app/dashboard/users/[id]/page.jsx
@@ -5,28 +5,30 @@ const SingleUserPage = () => {
   return (
     <div className='flex gap-12 mt-5'>
       <div className='flex flex-col gap-3 bg-slate-900 p-5 rounded-lg font-bold'>
-        <Image className='flex-1 rounded-lg' src='/profile.png' alt='profile picture' width='300' height='300' />
+        <Image className='flex rounded-lg' src='/profile.png' alt='profile picture' width='300' height='300' />
         <p>Charles V</p>
       </div>
-      <div className='flex-3 bg-slate-900 p-5 rounded-lg'>
-        <label>Username</label>
-        <input type='text' name='username' placeholder='Charles v' />
-        <label>Phone Number</label>
-        <input type='number' name='phone' placeholder='+25470000000' />
-        <label>Email</label>
-        <input type='email' name='email' placeholder='Charlesv@gmail.com' />
-        <label>Password</label>
-        <input type='password' name='password' placeholder='Charles' />
-        <label>Is Admin?</label>
-        <select name='isAdmin' id='isAdmin'>
-          <option value={true}>Yes</option>
-          <option value={false}>No</option>
-        </select>
-        <label>Is Active?</label>
-        <select name='isActive' id='isActive'>
-          <option value={true}>Yes</option>
-          <option value={false}>No</option>
-        </select>
+      <div className='flex-auto bg-slate-900 p-5 rounded-lg'>
+        <form className='flex flex-col'>
+          <label>Username</label>
+          <input className='p-5 w-full bg-transparent rounded-md border-2 border-slate-700' type='text' name='username' placeholder='Charles v' />
+          <label>Phone Number</label>
+          <input className='p-5 bg-transparent rounded-md border-2 border-slate-700' type='number' name='phone' placeholder='+25470000000' />
+          <label>Email</label>
+          <input className='p-5 bg-transparent rounded-md border-2 border-slate-700' type='email' name='email' placeholder='Charlesv@gmail.com' />
+          <label>Password</label>
+          <input className='p-5 bg-transparent rounded-md border-2 border-slate-700' type='password' name='password' placeholder='Charles' />
+          <label>Is Admin?</label>
+          <select className='p-5 bg-transparent rounded-md border-2 border-slate-700' name='isAdmin' id='isAdmin'>
+            <option value={true}>Yes</option>
+            <option value={false}>No</option>
+          </select>
+          <label>Is Active?</label>
+          <select className='p-5 bg-transparent rounded-md border-2 border-slate-700' name='isActive' id='isActive'>
+            <option value={true}>Yes</option>
+            <option value={false}>No</option>
+          </select>
+        </form>
       </div>
     </div>
   )
