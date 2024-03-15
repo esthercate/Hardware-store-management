e31d878e114ff65b69ff92e1a01c8074e3527f22
catherine <vuthi.esther@gmail.com>
2024-03-15 22:06:51 +0300

    remove password placeholder

--- app/dashboard/users/[id]/page.jsx
+++ app/dashboard/users/[id]/page.jsx
@@ -4,8 +4,8 @@ import Image from 'next/image'
 const SingleUserPage = () => {
   return (
     <div className='flex gap-10 mt-5'>
-      <div className='flex flex-col gap-3 font-bold'>
-        <Image className='flex rounded-lg' src='/profile.png' alt='profile picture' width='300' height='300' />
+      <div className='flex flex-col gap-4 font-bold'>
+        <Image className='relative overflow-hidden rounded-lg' src='/profile.png' alt='profile picture' width='300' height='300' />
         <p>Charles V</p>
       </div>
       <div className='flex-auto bg-slate-900 p-5 rounded-lg'>
@@ -17,7 +17,7 @@ const SingleUserPage = () => {
           <label>Email</label>
           <input className='p-5 my-2.5 bg-transparent rounded-md border-2 border-slate-700' type='email' name='email' placeholder='Charlesv@gmail.com' />
           <label>Password</label>
-          <input className='p-5 my-2.5 bg-transparent rounded-md border-2 border-slate-700' type='password' name='password' placeholder='Charles' />
+          <input className='p-5 my-2.5 bg-transparent rounded-md border-2 border-slate-700' type='password' name='password' />
           <label>Is Admin?</label>
           <select className='p-5 my-2.5 bg-transparent rounded-md border-2 border-slate-700' name='isAdmin' id='isAdmin'>
             <option value={true}>Yes</option>
@@ -28,6 +28,9 @@ const SingleUserPage = () => {
             <option value={true}>Yes</option>
             <option value={false}>No</option>
           </select>
+          <button className='w-full p-5 mt-2.5 bg-green-600 rounded-md hover:bg-green-700 cursor-pointer text-white border-0' type='submit'>
+            Update
+          </button>
         </form>
       </div>
     </div>
