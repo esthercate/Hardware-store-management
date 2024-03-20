bd8d3778aba87f95a6cb1e4e076b4d5a24a672bc
catherine <vuthi.esther@gmail.com>
2024-03-20 21:46:28 +0300

    Limit user number per page to 2

--- app/lib/data.js
+++ app/lib/data.js
@@ -7,7 +7,7 @@ export const fetchUsers = async (q, page) => {
   const ITEM_PER_PAGE = 2
   try {
     connectToDB()
-    const users = await User.find({ username: { $regex: regex } }).limit(ITEM_PER_PAGE)
+    const users = await User.find({ username: { $regex: regex } }).limit(ITEM_PER_PAGE).skip(ITEM_PER_PAGE *(page-1))
     return JSON.parse(JSON.stringify(users))
   } catch (error) {
     console.log(error)
