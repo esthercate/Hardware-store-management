f2b541173371288c2e45c423277ed58187e5b6ea
catherine <vuthi.esther@gmail.com>
2024-03-21 21:17:55 +0300

    hero description text

--- app/ui/dashboard/coming-soon/comingsoon.jsx
+++ app/ui/dashboard/coming-soon/comingsoon.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 const ComingSoon = () => {
   return <div className='text-teal-600'>
     <h1>We're Coming Soon</h1>
+    <p>Stay tuned for something Amazing</p>
+    <div>
+      <p></p>
+    </div>
   </div>
 }
 
