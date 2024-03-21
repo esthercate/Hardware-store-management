b3cb63abf5c0d2ebd5a8f15a20d37eb2be19e4c5
catherine <vuthi.esther@gmail.com>
2024-03-21 21:15:39 +0300

    coming soon h1

--- app/ui/dashboard/coming-soon/comingsoon.jsx
+++ app/ui/dashboard/coming-soon/comingsoon.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 
 const ComingSoon = () => {
-  return <div className='text-teal-600'>ComingSoon</div>
+  return <div className='text-teal-600'>
+    <h1>We're Coming Soon</h1>
+  </div>
 }
 
 export default ComingSoon
