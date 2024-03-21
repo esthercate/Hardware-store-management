7e9a0d0a3bab248affe0f68c40a05d3a557e7160
catherine <vuthi.esther@gmail.com>
2024-03-21 21:21:16 +0300

    hours, min, sec text

--- app/ui/dashboard/coming-soon/comingsoon.jsx
+++ app/ui/dashboard/coming-soon/comingsoon.jsx
@@ -7,8 +7,21 @@ const ComingSoon = () => {
       <p>Stay tuned for something Amazing</p>
       <div>
         <div>
-          <p>Days</p><span>399</span>
-       </div>
+          <p>Days</p>
+          <span>399</span>
+        </div>
+        <div>
+          <p>Hours</p>
+          <span>14</span>
+        </div>
+        <div>
+          <p>Minutes</p>
+          <span>32</span>
+        </div>
+        <div>
+          <p>Seconds</p>
+          <span>5</span>
+        </div>
       </div>
     </div>
   )
