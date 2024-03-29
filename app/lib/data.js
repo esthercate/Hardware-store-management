60235cff17fc07428b542a948ab592fbf77ab3e8
catherine <vuthi.esther@gmail.com>
2024-03-29 20:55:30 +0300

    Remove comments

--- app/lib/data.js
+++ app/lib/data.js
@@ -7,7 +7,6 @@ export const fetchUsers = async (q, page) => {
   const ITEM_PER_PAGE = 2
   try {
     connectToDB()
-    // const count = await User.find({ username: { $regex: regex } }).count()
     const users = await User.find({ username: { $regex: regex } }).limit(ITEM_PER_PAGE).skip(ITEM_PER_PAGE *(page-1))
     return JSON.parse(JSON.stringify(users))
   } catch (error) {
