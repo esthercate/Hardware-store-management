eebe8f84e3c6df607f793fbeb4fe5b6064e15d0b
catherine <vuthi.esther@gmail.com>
2024-04-02 22:42:03 +0300

    fetch products server action

--- app/lib/data.js
+++ app/lib/data.js
@@ -14,3 +14,18 @@ export const fetchUsers = async (q, page) => {
     throw new Error('Failed to fetch users!')
   }
 }
+
+export const fetchProducts = async (q, page) => {
+  const regex = new RegExp(q, 'i')
+  const ITEM_PER_PAGE = 2
+  try {
+    connectToDB()
+    const products = await Product.find({ title: { $regex: regex } })
+      .limit(ITEM_PER_PAGE)
+      .skip(ITEM_PER_PAGE * (page - 1))
+    return JSON.parse(JSON.stringify(products))
+  } catch (error) {
+    console.log(error)
+    throw new Error('Failed to fetch products!')
+  }
+}
