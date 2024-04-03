efe910d4ab91186db53b83ea2bbfad6930da0b2d
catherine <vuthi.esther@gmail.com>
2024-04-03 22:53:14 +0300

    create new user

--- app/lib/actions.js
+++ app/lib/actions.js
@@ -1,3 +1,13 @@
+import {User} from './models'
 export const addUser = async (FormData) => {
-  const {username, phone, email, password, isAdmin, isActive } = Object.fromEntries(FormData)
+  const { username, phone, email, password, isAdmin, isActive } = Object.fromEntries(FormData)
+  
+  try {
+    const newUser = new User({
+
+    })
+  } catch (error) {
+    console.log(error)
+    throw new Error('Failed to create user')
+  }
 }
