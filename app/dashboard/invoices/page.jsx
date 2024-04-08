bcce9fc5f9aefcdfcec6fd8b4efc32e607a96ee5
catherine <vuthi.esther@gmail.com>
2024-04-08 22:50:34 +0300

    invoices remove h1

--- app/dashboard/invoices/page.jsx
+++ app/dashboard/invoices/page.jsx
@@ -3,11 +3,8 @@ import React from 'react'
 const InvoicesPage = () => {
   return (
     <div>
-      <h1 className='text-2xl'>Invoices</h1>
-      <div>
-        <div>blue side bar</div>
-        <div>right section</div>
-      </div>
+      <div>blue side bar</div>
+      <div>right section</div>
     </div>
   )
 }
