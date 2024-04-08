7c28fdd247aee97b3c612e3114026dcac978536a
catherine <vuthi.esther@gmail.com>
2024-04-08 22:57:22 +0300

    side bar text

--- app/dashboard/invoices/page.jsx
+++ app/dashboard/invoices/page.jsx
@@ -7,6 +7,19 @@ const InvoicesPage = () => {
         <h1>TV Hardware</h1>
         <div>
           <span>To: </span>
+          <h2>Mrs. Joyce</h2>
+          <p>Phone Number</p>
+          <p>Email</p>
+        </div>
+        <div>
+          <h3>Payment Method</h3>
+          <p>Mpesa</p>
+          <span>Till No: 123123</span>
+          <span>Send Many: 070000000</span>
+        </div>
+        <div>
+          <h3>Terms & Conditions</h3>
+          <p>Lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>
         </div>
       </div>
       <div>right section</div>
