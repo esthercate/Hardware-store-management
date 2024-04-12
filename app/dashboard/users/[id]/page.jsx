fbe84489420ab95eedfd6052a2521c14dafb730a
catherine <vuthi.esther@gmail.com>
2024-04-12 22:35:59 +0300

    updated selected option

--- app/dashboard/users/[id]/page.jsx
+++ app/dashboard/users/[id]/page.jsx
@@ -9,7 +9,7 @@ const SingleUserPage = async ({ params }) => {
   return (
     <div className='flex gap-10 mt-5'>
       <div className='flex flex-col gap-4 font-bold'>
-        <Image className='relative overflow-hidden rounded-lg' src={user.img || '/profile.png' }alt='profile picture' width='300' height='300' />
+        <Image className='relative overflow-hidden rounded-lg' src={user.img || '/profile.png'} alt='profile picture' width='300' height='300' />
         <p>{user.username}</p>
       </div>
       <div className='flex-auto bg-slate-900 p-5 rounded-lg'>
@@ -25,13 +25,19 @@ const SingleUserPage = async ({ params }) => {
           <input className='p-5 my-2.5 bg-transparent rounded-md border-2 border-slate-700' type='password' name='password' />
           <label>Is Admin?</label>
           <select className='p-5 my-2.5 bg-transparent rounded-md border-2 border-slate-700' name='isAdmin' id='isAdmin'>
-            <option value={true}>Yes</option>
-            <option value={false}>No</option>
+            <option value={true} selected={user.isAdmin}>
+              Yes
+            </option>
+            <option value={false} selected={!user.isAdmin}>
+              No
+            </option>
           </select>
           <label>Is Active?</label>
           <select className='p-5 my-2.5 bg-transparent rounded-md border-2 border-slate-700' name='isActive' id='isActive'>
-            <option value={true}>Yes</option>
-            <option value={false}>No</option>
+            <option value={true} selected={user.isActive}>
+              Yes
+            </option>
+            <option value={false} selected={!user.isActive}>No</option>
           </select>
           <button className='w-full p-5 mt-2.5 bg-green-600 rounded-md hover:bg-green-700 cursor-pointer text-white border-0' type='submit'>
             Update
