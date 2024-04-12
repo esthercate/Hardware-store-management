ddc9ff55bcbce3b6bbd4315941e3051d1278eb19
catherine <vuthi.esther@gmail.com>
2024-04-12 22:28:51 +0300

    Update user placeholder

--- app/dashboard/users/[id]/page.jsx
+++ app/dashboard/users/[id]/page.jsx
@@ -4,7 +4,7 @@ import {fetchUser} from '../../../lib/data'
 
 const SingleUserPage = async ({ params }) => {
   const {id} = params
-  const user = fetchUser(id)
+  const user = await fetchUser(id)
   return (
     <div className='flex gap-10 mt-5'>
       <div className='flex flex-col gap-4 font-bold'>
@@ -14,11 +14,11 @@ const SingleUserPage = async ({ params }) => {
       <div className='flex-auto bg-slate-900 p-5 rounded-lg'>
         <form className='flex flex-col'>
           <label>Username</label>
-          <input className='p-5 my-2.5 w-full bg-transparent rounded-md border-2 border-slate-700' type='text' name='username' placeholder='Charles v' />
+          <input className='p-5 my-2.5 w-full bg-transparent rounded-md border-2 border-slate-700' type='text' name='username' placeholder={user.username} />
           <label>Phone Number</label>
-          <input className='p-5 my-2.5 bg-transparent rounded-md border-2 border-slate-700' type='number' name='phone' placeholder='+25470000000' />
+          <input className='p-5 my-2.5 bg-transparent rounded-md border-2 border-slate-700' type='number' name='phone' placeholder={user.phone} />
           <label>Email</label>
-          <input className='p-5 my-2.5 bg-transparent rounded-md border-2 border-slate-700' type='email' name='email' placeholder='Charlesv@gmail.com' />
+          <input className='p-5 my-2.5 bg-transparent rounded-md border-2 border-slate-700' type='email' name='email' placeholder={user.email} />
           <label>Password</label>
           <input className='p-5 my-2.5 bg-transparent rounded-md border-2 border-slate-700' type='password' name='password' />
           <label>Is Admin?</label>
