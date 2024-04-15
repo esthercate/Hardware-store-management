e0b3c5f5db608ba28df6d10511d7eb5f55ff76a3
catherine <vuthi.esther@gmail.com>
2024-04-15 21:48:09 +0300

    Add the left side of the login form

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -5,23 +5,28 @@ import {authenticate} from '../lib/actions'
 const Login = () => {
   return (
     <div className='w-screen h-screen flex items-center justify-center'>
-      <form action={authenticate} className='flex flex-col justify-center gap-7 bg-slate-900 rounded-lg p-12 w-[500px] h-[500px]'>
-        <h1 className='text-3xl font-bold mb-3 text-center'>Login</h1>
-        <input type='text' placeholder='Username' name='username' className='p-5 border-solid border-2 border-slate-700 rounded-md bg-transparent' />
-        <input type='password' placeholder='Password' name='password' className='p-5 border-solid border-2 border-slate-700 rounded-md bg-transparent' />
-        <button className='p-5 bg-green-600 rounded-md hover:bg-green-700 cursor-pointer text-white border-0 font-bold' type='submit'>
-          Login
-        </button>
-        <div className='flex justify-between text-xs'>
-          <div className='flex gap-2'>
-            <input type='checkbox' id='loggedin' name='loggedin' value='loggedin' />
-            <label>Keep me logged in</label>
-          </div>
-          <Link className='underline' href='/'>
-            Forgot password?
-          </Link>
+      <div className='flex bg-slate-900 rounded-lg p-12 w-3/4 h-[500px]'>
+        <div>
+          <h1 className='text-3xl font-bold mb-3 text-center'>Welcome!</h1>
         </div>
-      </form>
+        <form action={authenticate} className='flex flex-col justify-center gap-7 '>
+          <h1 className='text-3xl font-bold mb-3 text-center'>Login</h1>
+          <input type='text' placeholder='Username' name='username' className='p-5 border-solid border-2 border-slate-700 rounded-md bg-transparent' />
+          <input type='password' placeholder='Password' name='password' className='p-5 border-solid border-2 border-slate-700 rounded-md bg-transparent' />
+          <button className='p-5 bg-green-600 rounded-md hover:bg-green-700 cursor-pointer text-white border-0 font-bold' type='submit'>
+            Login
+          </button>
+          <div className='flex justify-between text-xs'>
+            <div className='flex gap-2'>
+              <input type='checkbox' id='loggedin' name='loggedin' value='loggedin' />
+              <label>Keep me logged in</label>
+            </div>
+            <Link className='underline' href='/'>
+              Forgot password?
+            </Link>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
