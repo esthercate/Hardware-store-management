70fa46ef7a0cf660cd1b36bdb429c78fe2bf3cde
catherine <vuthi.esther@gmail.com>
2024-04-15 21:53:43 +0300

    Add get started button

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -7,7 +7,11 @@ const Login = () => {
     <div className='w-screen h-screen flex items-center justify-center'>
       <div className='flex bg-slate-900 rounded-lg p-12 w-3/4 h-[500px]'>
         <div>
-          <h1 className='text-3xl font-bold mb-3 text-center'>Welcome!</h1>
+          <h1 className='text-3xl font-bold mb-3'>Welcome!</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce aliquam, lectus vel fringilla varius, neque lacus fermentum lectus</p>
+          <button className='py-2 px-3 border-2 border-green-600 rounded-md hover:bg-green-700 cursor-pointer text-white border-0 font-bold' type='submit'>
+            Get Started
+          </button>
         </div>
         <form action={authenticate} className='flex flex-col justify-center gap-7 '>
           <h1 className='text-3xl font-bold mb-3 text-center'>Login</h1>
