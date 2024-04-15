8a3cd0c7ef1a639945b590c6c446c6d32ad2260a
catherine <vuthi.esther@gmail.com>
2024-04-15 22:23:47 +0300

    Complete login page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -6,16 +6,16 @@ import loginImg from '../../public/login.svg'
 const Login = () => {
   return (
     <div className='w-screen h-screen flex items-center justify-center'>
-      <div className='flex bg-slate-900 rounded-lg p-12 w-3/4 h-[500px] gap-8'>
-        <div className="flex flex-col w-1/2 justify-center gap-6 bg-[url('../../public/login.svg')] bg-cover bg-no-repeat bg-center">
+      <div className='flex bg-slate-900 rounded-lg w-3/4 h-[500px] gap-8'>
+        <div className="flex flex-col w-1/2 p-8 justify-center gap-6 bg-[url('../../public/login.svg')] bg-cover bg-no-repeat bg-center">
           <h1 className='text-3xl font-bold'>Welcome!</h1>
           <div className='border-t-2 border-green-600 w-[50px]' />
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce aliquam, lectus vel fringilla varius.</p>
-          <button className='py-2 px-3 border-2 border-green-600 rounded-md hover:bg-green-700 cursor-pointer text-white border-0 font-bold w-1/2' type='submit'>
+          <p className='text-sm'>Manage stock inventory and invoices effortlessly to ensure seamless operations and meet customer demands efficiently.</p>
+          <button className='py-2 px-3 mt-2 border-2 border-green-600 rounded-md hover:bg-green-700 cursor-pointer text-white border-0 font-bold w-1/2' type='submit'>
             Get Started
           </button>
         </div>
-        <form action={authenticate} className='flex flex-col justify-center gap-7 w-1/2'>
+        <form action={authenticate} className='flex flex-col justify-center gap-7 w-1/2 p-8'>
           <h1 className='text-3xl font-bold mb-3'>Login</h1>
           <input type='text' placeholder='Username' name='username' className='p-5 border-solid border-2 border-slate-700 rounded-md bg-transparent' />
           <input type='password' placeholder='Password' name='password' className='p-5 border-solid border-2 border-slate-700 rounded-md bg-transparent' />
