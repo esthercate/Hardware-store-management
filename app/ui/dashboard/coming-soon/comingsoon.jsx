e5a39f7a16a9c0568eaa6698e7f45816530f9e94
catherine <vuthi.esther@gmail.com>
2024-04-23 23:03:44 +0300

    Style h1

--- app/ui/dashboard/coming-soon/comingsoon.jsx
+++ app/ui/dashboard/coming-soon/comingsoon.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const ComingSoon = () => {
   return (
     <div className='flex flex-col text-slate-400 gap-8 bg-slate-900 justify-center items-center p-5 rounded-lg mt-5'>
-      <h1>We`re Coming Soon</h1>
+      <h1 className='font-semibold text-2xl'>We`re Coming Soon</h1>
       <p>Stay tuned for something Amazing!</p>
       <div className='flex gap-5'>
         <div className='w-12 h-12 rounded-full border border-white'>
