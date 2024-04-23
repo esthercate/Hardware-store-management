97f49ac5b789401ec7f82e3d1f034e2e91171f38
catherine <vuthi.esther@gmail.com>
2024-04-23 23:01:59 +0300

    style changes

--- app/ui/dashboard/coming-soon/comingsoon.jsx
+++ app/ui/dashboard/coming-soon/comingsoon.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const ComingSoon = () => {
   return (
-    <div className='text-slate-400 flex flex-col gap-5'>
+    <div className='flex flex-col text-slate-400 gap-8 bg-slate-900 p-5 rounded-lg mt-5'>
       <h1>We`re Coming Soon</h1>
       <p>Stay tuned for something Amazing!</p>
       <div className='flex gap-5'>
