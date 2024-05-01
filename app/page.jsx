582a240dfc2efb4d0edb258d848c4c8ce526cc61
catherine <vuthi.esther@gmail.com>
2024-05-01 17:30:06 +0300

    Home page changes

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,14 @@
-export default function Home() {
-  return <div className='text-2xl text-white'>Home page</div>
+import Login from './login/page'
+
+
+export default function Home () {
+  return (
+    <div className='text-white'>
+      <div className='flex py-6 px-8 justify-between items-center w-full'>
+        <h1 className='text-green-600 font-bold text-xl'>TV Hardware Store</h1>
+        <p>Contact us: 0710 000 000</p>
+      </div>
+      <Login />
+    </div>
+  ) 
 }
