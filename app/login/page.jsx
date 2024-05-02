6a4a91fe3ec79465b282b999e7bfa01a2315918f
catherine <vuthi.esther@gmail.com>
2024-05-02 20:13:40 +0300

    Login page responsiveness

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -5,16 +5,16 @@ import {authenticate} from '../lib/actions'
 const Login = () => {
   return (
     <div className='w-screen h-screen flex items-center justify-center'>
-      <div className='flex bg-slate-900 rounded-lg w-3/4 h-[500px] gap-8'>
-        <div className="flex flex-col w-1/2 p-8 justify-center gap-6 bg-[url('../../public/login.svg')] bg-cover bg-no-repeat bg-center">
+      <div className='flex bg-slate-900 rounded-lg w-full md:w-3/4 h-[500px] gap-8 mx-2 md:mx-0'>
+        <div className="hidden md:flex flex-col w-1/2 p-8 justify-center gap-6 bg-[url('../../public/login.svg')] bg-cover bg-no-repeat bg-center">
           <h1 className='text-3xl font-bold'>Welcome!</h1>
           <div className='border-t-2 border-green-600 w-[50px]' />
           <p className='text-sm'>Manage stock inventory and invoices effortlessly to ensure seamless operations and meet customer demands efficiently.</p>
-          <button className='py-2 px-3 mt-2 border-2 border-green-600 rounded-md hover:bg-green-700 cursor-pointer text-white font-bold w-1/2' type='submit'>
+          <Link href='/' className='py-2 px-3 mt-2 border-2 border-green-600 rounded-md hover:bg-green-700 cursor-pointer text-white font-bold w-1/2'>
             Get Started
-          </button>
+          </Link>
         </div>
-        <form action={authenticate} className='flex flex-col justify-center gap-7 w-1/2 pr-8'>
+        <form action={authenticate} className='flex flex-col justify-center gap-7 w-full md:w-1/2 px-3 md:px-0 md:pr-8 '>
           <h1 className='text-3xl font-bold mb-3'>Login</h1>
           <input type='text' placeholder='Username' name='username' className='p-5 border-solid border-2 border-slate-700 rounded-md bg-transparent' />
           <input type='password' placeholder='Password' name='password' className='p-5 border-solid border-2 border-slate-700 rounded-md bg-transparent' />
--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,9 @@
 import Login from './login/page'
 
-
 export default function Home () {
   return (
     <div className='text-white'>
-      <div className='flex py-6 px-8 justify-between items-center w-full'>
+      <div className='flex pt-6 px-8 justify-between items-center'>
         <h1 className='text-green-600 font-bold text-xl'>TV Hardware Store</h1>
         <p>Contact us: 0710 000 000</p>
       </div>
