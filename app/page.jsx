60f477e6de87305387b2b544ecd70c502873565c
catherine <vuthi.esther@gmail.com>
2024-05-03 21:21:54 +0300

    remove navbar

--- app/page.jsx
+++ app/page.jsx
@@ -2,11 +2,7 @@ import Login from './login/page'
 
 export default function Home () {
   return (
-    <div className='text-white'>
-      <div className='flex pt-6 px-8 justify-between items-center'>
-        <h1 className='text-green-600 font-bold text-xl'>TV Hardware Store</h1>
-        <p>Contact us: 0710 000 000</p>
-      </div>
+    <div>
       <Login />
     </div>
   ) 
