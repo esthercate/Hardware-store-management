914b8123583f185f0ebbcd8de06ba3b58de43ae9
catherine <vuthi.esther@gmail.com>
2024-05-06 21:51:39 +0300

    Add h1

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,9 @@ import Login from './login/page'
 export default function Home () {
   return (
     <div className='flex gap-5'>
-      <div>text</div>
+      <div>
+        <h1>Manage your inventory easily</h1>
+      </div>
       <div> dashboard Images</div>
     </div>
   ) 
