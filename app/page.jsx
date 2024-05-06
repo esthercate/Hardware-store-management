44e99982733173787dade97d2ce3350e713c4cf5
catherine <vuthi.esther@gmail.com>
2024-05-06 21:53:12 +0300

    Insert a pararaph

--- app/page.jsx
+++ app/page.jsx
@@ -5,6 +5,7 @@ export default function Home () {
     <div className='flex gap-5'>
       <div>
         <h1>Manage your inventory easily</h1>
+        <p>Lorem int qui commodo velit voluptate ullamco fugiat. Ullamco labore Lorem aute eiusmod consequat. Enim nostrud proident fugiat consectetur. Cillum in dolor Lorem eu exercitation velit mollit consequat laboris. Dolore exercitation veniam Lorem enim Lorem. Ea magna incididunt cupidatat quis.</p>
       </div>
       <div> dashboard Images</div>
     </div>
