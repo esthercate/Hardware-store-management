36497eda64b6dcd1d0b20800c0210d6b0329e24b
catherine <vuthi.esther@gmail.com>
2024-05-06 21:54:44 +0300

    Add link to login

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,4 @@
-import Login from './login/page'
+import Link from 'next/link'
 
 export default function Home () {
   return (
@@ -6,6 +6,9 @@ export default function Home () {
       <div>
         <h1>Manage your inventory easily</h1>
         <p>Lorem int qui commodo velit voluptate ullamco fugiat. Ullamco labore Lorem aute eiusmod consequat. Enim nostrud proident fugiat consectetur. Cillum in dolor Lorem eu exercitation velit mollit consequat laboris. Dolore exercitation veniam Lorem enim Lorem. Ea magna incididunt cupidatat quis.</p>
+      </div>
+      <div>
+        <Link href='/login'>Get Started</Link>
       </div>
       <div> dashboard Images</div>
     </div>
