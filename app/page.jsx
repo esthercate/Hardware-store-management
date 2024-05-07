64e71b80d673c1b07580cff83fd661437629182e
catherine <vuthi.esther@gmail.com>
2024-05-07 22:27:40 +0300

    home page padding

--- app/page.jsx
+++ app/page.jsx
@@ -2,8 +2,8 @@ import Link from 'next/link'
 
 export default function Home () {
   return (
-    <div className='flex gap-5'>
-      <div className='flex flex-col gap-3'>
+    <div className='flex gap-10 p-12 w-full'>
+      <div className='flex flex-col gap-3 w-1/2'>
         <h1>Manage your inventory easily</h1>
         <p>
           Lorem int qui commodo velit voluptate ullamco fugiat. Ullamco labore Lorem aute eiusmod consequat. Enim nostrud proident fugiat consectetur. Cillum in dolor Lorem eu exercitation velit mollit consequat laboris. Dolore exercitation veniam
