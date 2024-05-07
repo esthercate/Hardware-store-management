19b711ac6a6eacd797f2896c210cc78d6e975a59
catherine <vuthi.esther@gmail.com>
2024-05-07 22:34:28 +0300

    Add navbar section

--- app/page.jsx
+++ app/page.jsx
@@ -1,22 +1,22 @@
 import Link from 'next/link'
 
-export default function Home () {
+export default function Home() {
   return (
-    <div className='flex gap-10 p-12 w-full'>
-      <div className='flex flex-col gap-3 w-1/2'>
-        <h1>Manage your inventory easily</h1>
-        <p>
-          Lorem int qui commodo velit voluptate ullamco fugiat. Ullamco labore Lorem aute eiusmod consequat. Enim nostrud proident fugiat consectetur. Cillum in dolor Lorem eu exercitation velit mollit consequat laboris. Dolore exercitation veniam
-          Lorem enim Lorem. Ea magna incididunt cupidatat quis.
-        </p>
-        <div>
-          <Link href='/login' className='py-2 px-3 mt-2 border-2 border-green-600 rounded-md hover:bg-green-700 cursor-pointer text-white font-bold w-1/2'>
-            Get Started
-          </Link>
+    <div className='flex flex-col p-12 w-full gap-10'>
+      <div>navbar</div>
+      <div className='flex gap-10 w-full'>
+        <div className='flex flex-col gap-6 w-1/2'>
+          <h1>Manage your inventory easily</h1>
+          <p>
+            Lorem int qui commodo velit voluptate ullamco fugiat. Ullamco labore Lorem aute eiusmod consequat. Enim nostrud proident fugiat consectetur. Cillum in dolor Lorem eu exercitation velit mollit consequat laboris.
+          </p>
+            <Link href='/login' className='py-2 px-3 mt-2 border-2 border-green-600 rounded-md hover:bg-green-700 cursor-pointer text-white text-center font-bold w-1/2'>
+              Get Started
+            </Link>
         </div>
-      </div>
 
-      <div> dashboard Images</div>
+        <div> dashboard Images</div>
+      </div>
     </div>
-  ) 
+  )
 }
