ad3ed3d5600337676bca6404693eeeba55254da3
catherine <vuthi.esther@gmail.com>
2024-05-07 22:41:53 +0300

    Style navbar

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,15 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div className='flex flex-col p-12 w-full gap-10'>
-      <div>navbar</div>
+      <div className='flex justify-between items-center'>
+        <span>TV Hardware Stores</span>
+        <div className='flex gap-6 justify-center items-center'>
+          <span>Contact: 0700000000</span>
+          <Link href='/login' className='py-1 px-3 border-2 border-green-600 rounded-md bg-green-600 hover:bg-green-700 cursor-pointer text-white text-center font-bold w-32'>
+            Login
+          </Link>
+        </div>
+      </div>
       <div className='flex gap-10 w-full'>
         <div className='flex flex-col gap-6 w-1/2'>
           <h1>Manage your inventory easily</h1>
