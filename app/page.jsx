2a0e4bd6a83afb01f99115df093b392cc5a5db47
catherine <vuthi.esther@gmail.com>
2024-05-08 21:46:59 +0300

    Text changes

--- app/page.jsx
+++ app/page.jsx
@@ -14,9 +14,9 @@ export default function Home() {
       </div>
       <div className='flex justify-center w-full h-full'>
         <div className='flex flex-col items-center gap-10 w-1/2'>
-          <h1 className='flex flex-col text-3xl font-bold'>Manage your stock <span>easier and simple</span></h1>
-          <p className='text-sm text-center'>Lorem int qui commodo velit voluptate ullamco fugiat. Ullamco labore Lorem aute eiusmod consequat. Enim nostrud proident fugiat consectetur. Cillum in dolor Lorem eu exercitation velit mollit consequat laboris.</p>
-          <Link href='/login' className='py-2 px-3 mt-2 border-2 border-green-600 rounded-md hover:bg-green-700 cursor-pointer text-white text-center font-bold w-1/2'>
+          <h1 className='flex flex-col text-4xl font-bold text-center'>Streamline your business <span>growth with ease.</span></h1>
+          <p className='text-sm text-center'>Lorem int qui commodo velit voluptate ullamco fugiat. Ullamco labore Lorem aute eiusmod consequat. Enim nostrud proident fugiat consectetur.</p>
+          <Link href='/login' className='py-2 px-3 mt-2 border-2 border-green-600 rounded-md hover:bg-green-700 cursor-pointer text-white text-center font-bold w-1/4'>
             Get Started
           </Link>
         </div>
