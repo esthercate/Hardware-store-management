c900778e7ad856d60bf514ac93490a74ccd9ae23
catherine <vuthi.esther@gmail.com>
2024-05-08 21:31:51 +0300

    Height changes

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className='flex flex-col px-12 py-6 w-full gap-10'>
+    <div className='flex flex-col px-12 py-6 w-full h-screen gap-10'>
       <div className='flex justify-between items-center'>
         <span className='text-green-700 text-lg font-bold'>TV Hardware Stores</span>
         <div className='flex gap-6 justify-center items-center'>
@@ -12,15 +12,13 @@ export default function Home() {
           </Link>
         </div>
       </div>
-      <div className='flex gap-10 w-full'>
-        <div className='flex flex-col gap-6 w-1/2'>
+      <div className='flex items-center gap-16 w-full h-full'>
+        <div className='flex flex-col gap-10 w-1/2'>
           <h1 className='text-3xl font-bold'>Manage your inventory easily</h1>
-          <p>
-            Lorem int qui commodo velit voluptate ullamco fugiat. Ullamco labore Lorem aute eiusmod consequat. Enim nostrud proident fugiat consectetur. Cillum in dolor Lorem eu exercitation velit mollit consequat laboris.
-          </p>
-            <Link href='/login' className='py-2 px-3 mt-2 border-2 border-green-600 rounded-md hover:bg-green-700 cursor-pointer text-white text-center font-bold w-1/2'>
-              Get Started
-            </Link>
+          <p>Lorem int qui commodo velit voluptate ullamco fugiat. Ullamco labore Lorem aute eiusmod consequat. Enim nostrud proident fugiat consectetur. Cillum in dolor Lorem eu exercitation velit mollit consequat laboris.</p>
+          <Link href='/login' className='py-2 px-3 mt-2 border-2 border-green-600 rounded-md hover:bg-green-700 cursor-pointer text-white text-center font-bold w-1/2'>
+            Get Started
+          </Link>
         </div>
 
         <div>Dashboard Images</div>
