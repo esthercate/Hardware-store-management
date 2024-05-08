1e35c4ff92735128c1d58c1770dd674b50ce1ee5
catherine <vuthi.esther@gmail.com>
2024-05-08 21:39:01 +0300

    Home page layout changes

--- app/page.jsx
+++ app/page.jsx
@@ -12,16 +12,14 @@ export default function Home() {
           </Link>
         </div>
       </div>
-      <div className='flex items-center gap-16 w-full h-full'>
-        <div className='flex flex-col gap-10 w-1/2'>
+      <div className='flex justify-center w-full h-full'>
+        <div className='flex flex-col items-center gap-10 w-1/2'>
           <h1 className='text-3xl font-bold'>Manage your inventory easily</h1>
-          <p>Lorem int qui commodo velit voluptate ullamco fugiat. Ullamco labore Lorem aute eiusmod consequat. Enim nostrud proident fugiat consectetur. Cillum in dolor Lorem eu exercitation velit mollit consequat laboris.</p>
+          <p className='text-sm text-center'>Lorem int qui commodo velit voluptate ullamco fugiat. Ullamco labore Lorem aute eiusmod consequat. Enim nostrud proident fugiat consectetur. Cillum in dolor Lorem eu exercitation velit mollit consequat laboris.</p>
           <Link href='/login' className='py-2 px-3 mt-2 border-2 border-green-600 rounded-md hover:bg-green-700 cursor-pointer text-white text-center font-bold w-1/2'>
             Get Started
           </Link>
         </div>
-
-        <div>Dashboard Images</div>
       </div>
     </div>
   )
